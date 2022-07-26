b2cdd479d9dfb6d9d601c43ea49430a84db42b77
eyphn-ky <eyuphan.kaya34@gmail.com>
2022-07-27 00:22:44 +0300

    installed json-server

--- src/App.js
+++ src/App.js
@@ -1,27 +1,33 @@
-import './App.css';
-import CategoryList from './CategoryList';
-import Navi from './Navi';
-import ProductList from './ProductList';
-import {Row,Col,Container} from 'reactstrap';
+import "./App.css";
+import CategoryList from "./CategoryList";
+import Navi from "./Navi";
+import ProductList from "./ProductList";
+import { Row, Col, Container } from "reactstrap";
+import React, { Component } from "react";
 
-function App() {
-  let categoryInfo={title:"Category List"};
-  let productInfo={title:"Product List"};
-  return (
-    <Container>
-      <Row>
-        <Navi/>
-      </Row>
-      <Row>
-        <Col xs="3">
-          <CategoryList info={categoryInfo}/>
-        </Col>
-        <Col xs="9">
-          <ProductList info={productInfo}/>
-        </Col>
-      </Row>
-    </Container>
-  );
+export default class App extends Component {
+  changeCategory=(category)=>{this.setState({currentCategory:category.categoryName})}
+  state={
+    currentCategory:""
+  }
+
+  render() {//fonksiyon komponent kullansaydık tek fonksiyon bu olduğundan başka fonksiyon yazamazdık. Bu yüzden class componente çevirdik App'i
+    let categoryInfo = { title: "Category List" };
+    let productInfo = { title: "Product List" };
+    return (
+      <Container>
+        <Row>
+          <Navi />
+        </Row>
+        <Row>
+          <Col xs="3">
+            <CategoryList currentCategory={this.state.currentCategory} changeCategory={this.changeCategory} info={categoryInfo} />
+          </Col>
+          <Col xs="9">
+            <ProductList currentCategory={this.state.currentCategory} info={productInfo} />
+          </Col>
+        </Row>
+      </Container>
+    );
+  }
 }
-
-export default App;
--- src/CategoryList.js
+++ src/CategoryList.js
@@ -6,10 +6,8 @@ export default class CategoryList extends Component {
       categories:[
         {categoryId:1, categoryName:"Beverages"},
         {categoryId:2, categoryName:"Condiments"},
-      ],
-      currentCategory:""
+      ]
     };
-  changeCategory=(category)=>{this.setState({currentCategory:category.categoryName})}
   render() {
     return (
       <div>
@@ -17,13 +15,13 @@ export default class CategoryList extends Component {
 <ListGroup>
   {
     this.state.categories.map(category=>
-      <ListGroupItem onClick={()=>this.changeCategory(category)} key={category.categoryId}> 
+      <ListGroupItem onClick={()=>this.props.changeCategory(category)} key={category.categoryId}> 
       {category.categoryName}
       </ListGroupItem>//key value şeklinde olmak zorunda
     )
   }
   </ListGroup>
-<h4>{this.state.currentCategory}</h4>
+<h4>{this.props.currentCategory}</h4>
       </div>
     )
   }
