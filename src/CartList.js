445f1217e60779b73f7728fa7570f4c1708c1612
eyphn-ky <eyuphan.kaya34@gmail.com>
2022-08-02 20:06:13 +0300

    remove from cart added to cart-list page

--- src/CartList.js
+++ src/CartList.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import {Table} from 'reactstrap'
+import {Table,Button} from 'reactstrap'
 
 export class CartList extends Component {
   renderCart(){
@@ -13,6 +13,7 @@ export class CartList extends Component {
               <th>Unit Price</th>
               <th>Units In Stock</th>
               <th>Quantity</th>
+              <th>Remove From Cart</th>
             </tr>
           </thead>
           <tbody>
@@ -26,6 +27,7 @@ export class CartList extends Component {
                   <td>{cartItem.product.unitPrice}</td>
                   <td>{cartItem.product.unitsInStock}</td>
                   <td>{cartItem.quantity}</td>
+                  <td><Button className='danger' onClick={()=>this.props.removeFromCart(cartItem.product)}>Remove</Button></td>
                 </tr>
                )})
             }
